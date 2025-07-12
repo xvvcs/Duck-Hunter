f7d13bb2f2630dfa870c147a7e2c081dace7777b
xvvcs <maciek.m2004@wp.pl>
2025-07-12 20:45:07 +0200

    game sprites added

--- src/main.ts
+++ src/main.ts
@@ -49,6 +49,34 @@ k.scene("game", () => {
   k.setCursor("none");
   k.add([k.rect(k.width(), k.height()), k.color(COLORS.BLUE), "sky"]);
   k.add([k.sprite("background"), k.pos(0, -10), k.z(1)]);
+
+  const score = k.add([
+    k.text(formatScore(0), { font: "nes-font", size: 8 }),
+    k.pos(192, 197),
+    k.z(2),
+    k.color(COLORS.WHITE),
+  ]);
+
+  const roundCount = k.add([
+    k.text("1", { font: "nes-font", size: 8 }),
+    k.pos(42, 181),
+    k.z(2),
+    k.color(COLORS.RED),
+  ]);
+
+  const duckIcons = k.add([k.pos(95, 198)]);
+  let duckIconPosX = 1;
+  for (let i = 0; i < 10; i++) {
+    duckIcons.add([k.rect(7, 9), k.pos(duckIconPosX, 0), `duck-icon-${i}`]);
+    duckIconPosX += 8;
+  }
+
+  const bulletUIMask = k.add([
+    k.rect(0, 8),
+    k.pos(25, 198),
+    k.z(2),
+    k.color([0, 0, 0]),
+  ]);
 });
 
 k.scene("game-over", () => {});
